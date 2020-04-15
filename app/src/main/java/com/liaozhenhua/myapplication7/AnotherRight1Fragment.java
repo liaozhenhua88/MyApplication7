062f788f5843e2622caed6256592bafdae297131
liaozhenhua <1376455707@qq.com>
2020-04-15 15:32:20 +0800

    这是7工程

---
+++ app/src/main/java/com/liaozhenhua/myapplication7/AnotherRight1Fragment.java
@@ -0,0 +1,21 @@
+package com.liaozhenhua.myapplication7;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AnotherRight1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_another_right1, container, false);
+    }
+
+}
+
+
+
+
